e524204352b82639af080ddc9aea1d3aba470b28
Vish <savishy.pv@gmail.com>
2017-02-17 12:44:57 +0530

    #11 docker-machine will now be installed - cleaned up old effort ubuntu1604-devops box to avoid confusion.
    - additional mounting of workspace directory
    - additional ports forwarded

--- docker-host/environments/production/modules/install_docker/manifests/init.pp
+++ docker-host/environments/production/modules/install_docker/manifests/init.pp
@@ -76,4 +76,11 @@ class install_docker {
     remote_location => 'https://github.com/docker/compose/releases/download/1.8.0/docker-compose-Linux-x86_64',
     mode            => '0777',
   }
+
+  # download docker machine
+  common::remote_file{'/usr/local/bin/docker-machine':
+    remote_location => 'https://github.com/docker/machine/releases/download/0.9.0/docker-machine-Linux-x86_64',
+    mode            => '0777',
+  }
+
 }
